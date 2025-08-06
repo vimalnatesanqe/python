2765409227bdf74f54a8906fda7ee0cedc5be2f3
vimalnatesanqe <vimalnatesan.qe@gmail.com>
2025-08-06 13:33:09 +0530

    keyword topic completed

---
+++ 0002.keywords.py
@@ -0,0 +1,11 @@
+#keywords
+
+""" This is a comment explaining the code below---
+
+What is a keyword?
+Keywords are reserved words in Python that have special meaning and cannot be used as variable names."""
+
+import keyword
+# Display all keywords in Python
+print(keyword.kwlist)
+ 
