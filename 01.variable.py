537838beef4386e88b9a46990e88d99bf6206eaa
vimalnatesanqe <vimalnatesan.qe@gmail.com>
2025-08-06 13:18:08 +0530

    varible notes completed

--- 01.variable.py
+++ 01.variable.py
@@ -1,1 +1,29 @@
-#basic
+#variable declaration and assignment
+""" This is a comment explaining the code below---
+
+What is a variable?
+variable is a memory location which we can use to store data in RAM
+
+Rules:
+1. Variable names can only contain letters, numbers, and underscores.
+2. Variable names cannot start with a number and special characters are not allowed.
+3. Variable names are case-sensitive.
+4. Variable names should be descriptive and meaningful
+5.variable should not contains keywords. 
+"""
+# Variable declaration
+my_name="vimal" 
+"""Name is variable name and "vimal" string data type value is assigned to it"""
+age=35
+my_age=36
+
+print(my_age)
+print(my_name)
+print(age)
+print(type(my_name))
+print(type(age))
+
+# better way to declare variable name
+my_name = "vimal"
+my_age = 36 # my_name and my_age are variable names
+print(my_name)  
