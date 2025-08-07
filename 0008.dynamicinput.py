9af913ef52cb01f0ac5abb7d311387553c3561c1
vimalnatesanqe <vimalnatesan.qe@gmail.com>
2025-08-07 14:00:50 +0530

    user input completed

---
+++ 0008.dynamicinput.py
@@ -0,0 +1,9 @@
+#user input recived by console
+
+user_name=input("enter your name::\n".strip()) #default it is consider a string
+print(f"hey {user_name} welcome to nallas".center(100,"-"))
+
+# to get int values from users by type casting
+
+user_age=int(input("Enter your age::\n"))
+print(f"hey {user_name} your age is {user_age}")
