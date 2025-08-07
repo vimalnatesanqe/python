8b90e1620e7f23439771309d3e2903c3a7691038
vimalnatesanqe <vimalnatesan.qe@gmail.com>
2025-08-07 11:34:16 +0530

    type casting completed

---
+++ 0005.typecasting.py
@@ -0,0 +1,42 @@
+#type casting
+#convert a data type one to another data type is called type casting
+
+#implicit type casting
+#explicit type casting
+
+#implicit type casting
+
+"python will convert data type by it self"
+
+#explicit type casting
+
+"programmer will convert the data type one to another"
+
+
+#int type cast
+print(int(10.00))
+#complex not possible
+#str is possible when it contain numeric value base 10.. not float value
+print(int('10'))
+#bool
+print(int(True))
+
+#float type cast
+print(float(10))
+print(float('10'))
+print(float(True))
+
+#bool type cast applicable for all data type
+
+int
+print(bool(10))
+print(bool(0))
+float
+print(bool(10.0))
+print(bool(0.0))
+complex
+print(bool(10+4j))
+print(bool(0+0j))
+str
+print(bool('this is a string'))
+print(bool(''))
