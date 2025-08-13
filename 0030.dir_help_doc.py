69050da7055b8c5546c40c285dedac3027f5ad0f
vimalnatesanqe <vimalnatesan.qe@gmail.com>
2025-08-13 12:18:25 +0530

    dir,help,__doc__

---
+++ 0030.dir_help_doc.py
@@ -0,0 +1,11 @@
+# using dir function we can know about memberes information inside of module
+
+#using help function we can know about members detatils and description
+
+import modules
+
+print(dir(modules))
+
+print(help(modules))
+
+print(modules.__doc__)
